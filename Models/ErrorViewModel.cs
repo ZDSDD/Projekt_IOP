ed2a6b169556433cfe5aeeb077b5d3df09a8c2b8
ZDSDD <sebasadowy@gmail.com>
2023-03-09 22:21:14 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudSentials.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
